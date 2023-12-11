0a6b030c243fbc29a0f845c512ad2e7cda662c85
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-11 17:37:50 +0530

    Create printsentence.py

---
+++ printsentence.py
@@ -0,0 +1,17 @@
+x = "Python"
+y = "is"
+z = "awesome"
+
+print(x, y, z)
+print(x + y + z)
+
+
+a = 5
+b = "apples"
+print(a, b)
+
+"""
+a = 5
+b = "apples"
+print(a + b)    <-- This will give error
+"""
