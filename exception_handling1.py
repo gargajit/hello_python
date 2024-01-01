9909afd93b906a161ebc3d5ef6bec571bc7dc097
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-01 21:54:59 +0530

    Update exception_handling1.py

--- exception_handling1.py
+++ exception_handling1.py
@@ -1,4 +1,4 @@
-#One way
+# One way
 '''
 while True:
     try:
@@ -11,7 +11,7 @@ while True:
 print(f"x is {x}")
 '''
 
-#Second Way - No need for else
+# Second Way - No need for else
 '''
 while True:
     try:
@@ -23,8 +23,10 @@ while True:
 print(f"x is {x}")
 '''
 
-# Creating a get_int() function
+# Third way
 
+# Creating a get_int() function
+'''
 def main():
     x = get_int()
     print(f"x is {x}")
@@ -32,10 +34,32 @@ def main():
 def get_int():
     while True:
         try:
-            x = int(input("What's x? "))
-            break
+            return int(input("What's x? "))
         except ValueError:
-            print("x is not an integer")
-    return x
+            #print("x is not an integer")
+            pass #**
+
+main()
+
+# ** If we do not want to prompt the exception but continue the loop
+# i.e. instead of throwing message that x is not an integer we want to ask what's x
+'''
+
+
+# Fourth Way
+
+# Let's refine the same program where the called function doesn't specify on it's own
+# rather would take the prompt from the main itself
+
+def main():
+    x = get_int("What's x? ")       # now main has the prompt of What's x
+    print(f"x is {x}")
+
+def get_int(prompt):                # take prompt itself as the argument
+    while True:
+        try:
+            return int(input(prompt))   #this will print the prompt asked in main
+        except ValueError:
+            pass
 
 main()
