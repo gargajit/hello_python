ce69a3c397a4295a903ca1e775fb76836c2c1afe
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-03-27 23:40:24 +0530

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -2,7 +2,7 @@ def main():
     r = float(input("radius: "))
     pi = 3.14
     area = pi * pow(r, 2)
-    print("Area of circle:", area)
+    print(f"Area of circle: {area:.2f}")
 
 if __name__ == "__main__":
     main()
