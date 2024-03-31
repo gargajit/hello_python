381f3bcfc8ce9c99bb57869b8449fd3569ca730e
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-03-31 15:06:31 +0530

    Update sum_zero_if_two_same.py

--- sum_zero_if_two_same.py
+++ sum_zero_if_two_same.py
@@ -1,20 +1,17 @@
 '''Write a Python program to sum three given integers. However, if two values are equal, the sum will be zero'''
 
 def main():
-    n1 = int(input("1st number: "))
-    n2 = int(input("2nd number: "))
-    n3 = int(input("3rd number: "))
+    x = int(input("1st number: "))
+    y = int(input("2nd number: "))
+    z = int(input("3rd number: "))
 
     sum = 0
 
-    if (n1 == n2 or n1 == n3) and n2 != n3:
+    if x == y or y == z or x == z: 
         print(f"Sum = {sum}")
 
-    elif (n2 == n1 or n2 == n3) and n1 != n3:
-        print(f"Sum = {sum}")
-        
     else:
-        sum = n1 + n2 + n3
+        sum = x + y + z
         print(f"Sum = {sum}")
 
 
