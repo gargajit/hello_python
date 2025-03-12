256e4ef4d49ea5d54b91d322936a2556cc2d7a79
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-03-12 23:26:40 +0530

    Create docstring_example.py

---
+++ docstring_example.py
@@ -0,0 +1,11 @@
+def add_two(a, b):
+  """
+  This function adds two integers and returns the sum
+  a: first integer
+  b: second integer
+  return: sum of a and b
+  """
+  return a + b
+
+
+help(add_two)
