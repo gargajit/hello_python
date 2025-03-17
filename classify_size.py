83dc215e5ddcd3f4cd5413cea0b886d6676fed1b
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-03-17 23:54:18 +0530

    Create classify_size.py

---
+++ classify_size.py
@@ -0,0 +1,12 @@
+l = [15, 25, 35]
+
+def classify_size(size):
+  if 10 <= size <= 19:
+    return "small"
+  elif 20 <= size <= 29:
+    return "medium"
+  elif 30 <= size <= 39:
+    return "large"
+  
+ans = [classify_size(size) for size in l]
+print(ans)
