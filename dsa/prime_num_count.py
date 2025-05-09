44a003d396f9aafa72bd9e2bfd56af34f523cd3c
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-05-09 10:53:03 +0530

    Create prime_num_count.py

---
+++ dsa/prime_num_count.py
@@ -0,0 +1,32 @@
+# How many Prime Numbers are there?
+
+def main():
+  l = list(map(int, input("Enter the Numbers: ").split()))
+  prime_nos = num_of_primes(l)
+  print(f"In the given list of numbers, the number of prime numbers are: {prime_nos}")
+
+def num_of_primes(ls):
+  cnt = 0
+  i = 0
+  while i < len(ls):
+    is_prime = True
+    if ls[i] < 2:
+      is_prime = False
+
+    else:
+       j = 2
+       while j * j <= ls[i]:
+        if ls[i] % j == 0:
+          is_prime = False
+          break
+        j += 1
+
+    if is_prime:
+      cnt += 1
+
+    i += 1
+
+  return cnt
+
+if __name__ == "__main__":
+  main()
