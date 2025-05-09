a925a6c4ca30cf7ca2184e6e1bedd93882043cf4
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-05-09 10:52:41 +0530

    Update prime_numbers.py

--- dsa/prime_numbers.py
+++ dsa/prime_numbers.py
@@ -1,33 +1,28 @@
-# How many Prime Numbers are there?
+# Print Prime Numbers
 
-def main():
-  l = list(map(int, input("Enter the Numbers: ").split()))
-  prime_nos = num_of_primes(l)
-  print(f"In the given list of numbers, the number of prime numbers are: {prime_nos}")
+ls = [1, 10, 7, 23, 2, 25, 27, 31]
 
-def num_of_primes(ls):
-  cnt = 0
-  i = 0
-  while i < len(ls):
-    is_prime = True
-    if ls[i] < 2:
+n = len(ls)
+
+
+primes = []
+
+for i in range(n):
+  is_prime = True
+  if ls[i] < 2:
+    is_prime = False
+
+  f = 2
+  while f * f <= ls[i]:
+    if ls[i] % f == 0:
       is_prime = False
+      break
+    f += 1
+      
+      
 
-    else:
-       j = 2
-       while j * j <= ls[i]:
-        if ls[i] % j == 0:
-          is_prime = False
-          break
-        j += 1
+  if is_prime:
+    primes.append(ls[i])
 
-    if is_prime:
-      cnt += 1
-
-    i += 1
-
-  return cnt
-
-if __name__ == "__main__":
-  main()
+print(primes)
 
